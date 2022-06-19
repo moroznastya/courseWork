ab89993b3866395d3cbf631a91994e86f6b2e3f2
moroznastya <nastya.moroz2210@gmail.com>
2022-06-19 13:41:43 +0300

    added tests

--- src/test/java/ua/lviv/iot/weatherStationServer/datastorage/WeatherStationDataFileStorageTest.java
+++ src/test/java/ua/lviv/iot/weatherStationServer/datastorage/WeatherStationDataFileStorageTest.java
@@ -1,7 +1,9 @@
 package ua.lviv.iot.weatherStationServer.datastorage;
 
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -11,32 +13,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class WeatherStationDataFileStorageTest {
 
-    @Test
-    void writeCSV() throws IOException {
-        try (FileReader expectedReader = new FileReader("src/test/resources/expectedResult.csv");
-             BufferedReader expectedBR = new BufferedReader(expectedReader);
-             FileReader actualReader = new FileReader("result.csv");
-             BufferedReader actualBR = new BufferedReader(actualReader);) {
-            Assertions.assertEquals(expectedBR.readLine(), actualBR.readLine());
+    public String getAllLines(String fileName) throws IOException {
+        FileReader fr = new FileReader(fileName);
+        BufferedReader br = new BufferedReader(fr);
+        String line;
+        StringBuilder allLines = new StringBuilder();
+        while ((line = br.readLine()) != null) {
+            allLines.append(line);
         }
+        return allLines.toString();
     }
 
-//    public String getAllLines(String fileName) throws IOException {
-//        FileReader fr = new FileReader(fileName);
-//        BufferedReader br = new BufferedReader(fr);
-//        String line;
-//        StringBuilder allLines = new StringBuilder();
-//        while ((line = br.readLine()) != null) {
-//            allLines.append(line);
-//        }
-//        return allLines.toString();
-//    }
-//
-//    @Test
-//    public void writeCSV()throws IOException{
-//        Assertions.assertEquals(getAllLines("src/test/resources/expectedResult.csv"),
-//                getAllLines("result.csv"));
+    @org.junit.jupiter.api.Test
+    public void saveWeatherStationData()throws IOException{
+        Assertions.assertEquals(getAllLines
+                        ("C:\\Users\\Admin\\IdeaProjects\\weatherStationServerr\\weatherStationServer" +
+                                "\\src\\test\\resources\\expectedWeatherStationData.csv"),
+                getAllLines("C:\\Users\\Admin\\IdeaProjects\\weatherStationServerr\\files" +
+                        "\\weatherStationData-2022-06-17.csv"));
 
-    //}
-
+    }
 }
